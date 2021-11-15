98d2bcb67cbe2c31b47fe205109924c947f81f1a
Abolfazl Motamed <motamedabolfazzll@gmail.com>
2021-11-15 13:12:27 -0800

    new node js commit

--- TestFirstProject/resources/views/add_role.blade.php
+++ TestFirstProject/resources/views/add_role.blade.php
@@ -1,5 +1,9 @@
 <html>
-   <head></head>
+   <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">     
+   </head>
 
    <body>
        add_role
