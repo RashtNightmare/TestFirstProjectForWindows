a1a9dcfcd6a1a9c5e14351fcaf960340acacde5e
Abolfazl Motamed <motamedabolfazzll@gmail.com>
2021-11-15 23:20:19 -0800

    new Commit

--- TestFirstProject/resources/views/add_role.blade.php
+++ TestFirstProject/resources/views/add_role.blade.php
@@ -15,19 +15,21 @@
    </body> -->
 
 <!-- </html> --> 
-
-<div class="w-full max-w-xs">
+<html>
 <head>
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">     
-   </head>  
-<form method="get" action="/role" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-        Username
-      </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+   </head> 
+   <body>
+    <div class="w-full max-w-xs" style="text-align: left;">
+   <form method="get" action="/role" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+   {{ csrf_field() }}      
+     <div class="mb-4">
+       <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+         Name
+        </label>
+       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Name">
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
@@ -47,7 +49,7 @@
         </div>
 
     <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Sign In
       </button>
     </div>
@@ -56,6 +58,7 @@
     &copy;2020 Acme Corp. All rights reserved.
   </p>
 
-  
+ 
 </div>
-
+</body>
+</html>
